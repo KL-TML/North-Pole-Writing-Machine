8ca0eb50f3ecdbfdba755e0f110539bdfd491ffd
KYLE LIPTON <lipton.kyle@gmail.com>
2017-11-15 20:27:53 -0500

    second commit

--- naughty_letter_writer.rb
+++ naughty_letter_writer.rb
@@ -6,14 +6,21 @@ naughty_letter = File.read('templates/naughty_letter_template.txt.erb')
 kids_data.each_line do |kid|
 
   kid_data_array = kid.split
-
+  # p kid
   name     = kid_data_array[0]
   behavior = kid_data_array[1]
   toys     = kid_data_array[2..7]
-  infraction = kid_data_array.slice_after("|").to_a[1]
-  if infraction.class == Array
-    infraction=infraction.join(" ")
+  infraction = kid.split('|')[1]
+  if infraction != nil
+    infraction = infraction.delete "\n"
+    p infraction
   end
+  # # infraction = kid_data_array.slice_after("|").to_a[1]
+  # # if infraction == Array
+  # #   infraction=infraction.join(" ")
+  # #   p infraction
+  #
+  # end
 
   next unless behavior == 'naughty'
 
